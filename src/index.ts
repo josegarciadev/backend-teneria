9952121726ac922ad999f3e9bebbda438c92f4c9
Jose Garcia <caskintero@gmail.com>
2020-11-15 22:03:17 -0400

    Primer commit, brackend para el sistema de la empresa teneria rubio.

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, {Application} from 'express';
+
+class Server{
+    public app : Application;
+    constructor(){
+        this.app= express();
+    }
+
+    config(){
+
+    }
+
+    routes(){
+
+    }
+
+    start(){
+
+    }
+}
+
+new Server();
