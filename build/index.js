1b599e530e58418f3df6db46384816d71dd7821c
Jose Garcia <caskintero@gmail.com>
2020-11-15 22:21:38 -0400

    Empezando a configurar las rutas, servidor funcionando localmente en el puerto 3000

--- build/index.js
+++ build/index.js
@@ -4,15 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 class Server {
     constructor() {
         this.app = express_1.default();
+        this.config();
+        this.routes();
     }
     config() {
+        this.app.set('port', process.env.PORT || 3000);
     }
     routes() {
+        this.app.use('', indexRoutes_1.default);
     }
     start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log("Servidor ON, Port: " + this.app.get('port'));
+        });
     }
 }
-new Server();
+const server = new Server();
+server.start();
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 import express, {Application} from 'express';
+import indexRoutes from './routes/indexRoutes';
 
 class Server{
     public app : Application;
     constructor(){
         this.app= express();
+        this.config();
+        this.routes();
     }
 
-    config(){
-
+    config():void{
+        this.app.set('port',process.env.PORT || 3000);
     }
 
-    routes(){
-
+    routes():void{
+        this.app.use('',indexRoutes);
     }
 
-    start(){
-
+    start():void{
+        this.app.listen(this.app.get('port'),()=>{
+            console.log("Servidor ON, Port: "+ this.app.get('port'));
+        });
     }
 }
 
-new Server();
+const server = new Server();
+server.start();
